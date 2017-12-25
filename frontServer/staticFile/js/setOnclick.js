a7998a9082fc48b11d3d9caa5668f7d1308925cc
VanxeHus <13751378996@163.com>
2017-12-25 11:39:20 +0800

    添加按钮相应代码

--- frontServer/staticFile/js/setOnclick.js
+++ frontServer/staticFile/js/setOnclick.js
@@ -1,17 +1,27 @@
-function setOnclick() {
+var btnOnclick = {};
+btnOnclick.SetHB_onclick = function () {
     var hideBtn = document.getElementById("hideBtn");
     console.log("on");
-    hideBtn.onclick = hideBtn_onClick();
+    hideBtn.onclick = btnOnclick.hideBtn_onclick();
 }
-function hideBtn_onClick() {
+btnOnclick.SetMenu_onclick = function () {
+    var menuList = document.getElementById("menuMx").children;
+    var i;
+    for (i = 0; i < menuList.length; ++i) {
+        menuList[i].onclick = btnOnclick.meneBtn_onclick(i);
+        console.log(menuList[i].children[0].pathname);
+    }
+
+}
+btnOnclick.hideBtn_onclick = function () {
     var containerOrigin = "container";
     var containerHide_menu = "container hideMenu";
     var containerHide_abst = "container hideMenu hideAbst";
 
-    var btnHide="hideMenu_btn";
-    var btnShow="hideMenu_btn hideMenu_btn1"
+    var btnHide = "hideMenu_btn";
+    var btnShow = "hideMenu_btn hideMenu_btn1"
     var container = document.getElementById("main");
-    var hideBtn=document.getElementById("hideBtn");
+    var hideBtn = document.getElementById("hideBtn");
     var menuStatus = 2;
     var btnStatus = 1;
     if (container.className.length == 9)
@@ -25,27 +35,45 @@ function hideBtn_onClick() {
         btnStatus = 0;
     else if (menuStatus == 2)
         btnStatus = 1;
-    console.log("menu:"+menuStatus+"btns:"+btnStatus);
+    console.log("menu:" + menuStatus + "btns:" + btnStatus);
     return function () {
         if (menuStatus == 2) {
             container.className = containerHide_menu;
-            menuStatus=1;
+            menuStatus = 1;
         } else if (menuStatus == 1 && btnStatus == 1) {
             container.className = containerHide_abst;
-            menuStatus=0;
-            btnStatus=0;
-            hideBtn.className=btnShow;
-        }else if(menuStatus==1&&btnStatus==0){
-            container.className=containerOrigin;
-            menuStatus=2;
-            btnStatus=1;
-            hideBtn.className=btnHide;
-        }else if(menuStatus==0){
-            container.className=containerHide_menu;
-            menuStatus=1;
+            menuStatus = 0;
+            btnStatus = 0;
+            hideBtn.className = btnShow;
+        } else if (menuStatus == 1 && btnStatus == 0) {
+            container.className = containerOrigin;
+            menuStatus = 2;
+            btnStatus = 1;
+            hideBtn.className = btnHide;
+        } else if (menuStatus == 0) {
+            container.className = containerHide_menu;
+            menuStatus = 1;
         }
-        console.log("ment:"+menuStatus+"btn:"+btnStatus);
+        console.log("ment:" + menuStatus + "btn:" + btnStatus);
         console.log("inside");
     }
 }
-setOnclick();
+btnOnclick.meneBtn_onclick = function (num) {
+    return function () {
+        var menuMx = document.getElementById("menuMx");
+        var menuList = menuMx.children;
+        var liActive = "liActive";
+        var liUnactive = "";
+        var i;
+        for (i = 0; i < menuList.length; ++i) {
+            if (i === num)
+                menuList[i].className = liActive;
+            else
+                menuList[i].className = liUnactive;
+        }
+        var coverLayer=document.getElementById("coverLayer");
+        coverLayer.className+=" containCover_layerLoad";
+        return false;
+    }
+}
+export { btnOnclick };
