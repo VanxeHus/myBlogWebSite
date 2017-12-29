171ce4f1299ee4431a5db74293ee3ec65b5b0ab7
VanxeHus <13751378996@163.com>
2017-12-29 17:50:17 +0800

    添加测试

--- frontServer/staticFile/js/app.js
+++ frontServer/staticFile/js/app.js
@@ -1,4 +1,7 @@
-import {view} from "../../view/renderView.js"
-import {ajax} from "../../ajax/getData.js"
-
-ajax.GetData("GET","/program",view.RenderArticle);
+//import { view } from "../../view/renderView.js"
+//import { ajax } from "../../ajax/getData.js"
+import { btnOnclick } from "./setOnclick.js"
+//ajax.GetArti_data("GET","/program",view);
+btnOnclick.SetMenu_onclick();
+btnOnclick.SetHB_onclick();
+//ajax.GetAbst_data("get","/program",view);
